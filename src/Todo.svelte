4bad2d72c50572f9f31f83c2066426985668ce7a
Preethi <preethi@distill.io>
2021-08-08 23:41:48 +0530

    basic structure

---
+++ src/Todo.svelte
@@ -0,0 +1,13 @@
+<script>
+</script>
+
+<div class="h-auto w-80 flex flex-col border-2">
+    <div class="">TODO</div>
+    <div class="flex flex-row justify-center items-center">
+        <div class="m-2 ">Add your task</div>
+        <button class="h-6 w-6 justify-center items-center">+</button>
+    </div>
+    <div class="h-auto w-full border-2 flex flex-col">
+        <input type="checkbox" />
+    </div>
+</div>
