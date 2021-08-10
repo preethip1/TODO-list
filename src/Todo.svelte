0b147925dddb2f3a94503624c32eb412ca308b91
Preethi <preethi@distill.io>
2021-08-10 22:02:05 +0530

    added elements to the list

--- src/Todo.svelte
+++ src/Todo.svelte
@@ -1,13 +1,37 @@
 <script>
+    let taskList = [];
+    let newTaskValue = "";
+    function addToList() {
+        taskList.push(newTaskValue);
+        taskList = taskList;
+        for (let i = 0; i < taskList.length; i++) {
+            console.log(taskList[i]);
+        }
+        newTaskValue = "";
+    }
 </script>
 
 <div class="h-auto w-80 flex flex-col border-2">
     <div class="">TODO</div>
-    <div class="flex flex-row justify-center items-center">
-        <div class="m-2 ">Add your task</div>
-        <button class="h-6 w-6 justify-center items-center">+</button>
+    <div class="flex flex-row justify-center items-center border-2 m-1 p-1">
+        <input
+            bind:value={newTaskValue}
+            type="text"
+            placeholder="Add your task "
+            class="border-none p-1"
+        />
+        <button
+            on:click={addToList}
+            class="h-7 w-7 border-none justify-center items-center">+</button
+        >
     </div>
-    <div class="h-auto w-full border-2 flex flex-col">
-        <input type="checkbox" />
+    <div class="h-auto w-full border-2 flex flex-col mt-4 p-2">
+        {#each taskList as task, index}
+            <div class="flex flex-row ">
+                <input type="checkbox" class="m-1 " />
+                <div type="text" class="border-2 text-red-900 m-1">{task}</div>
+                <div>x</div>
+            </div>
+        {/each}
     </div>
 </div>
